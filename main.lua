627a2c89c4a8aa763787d92c21df07f03efd4253
Laurent Zubiaur <lau.zubiaur@gmail.com>
2017-04-22 12:44:43 +0900

    Add sensor,edge,chain

--- main.lua
+++ main.lua
@@ -10,11 +10,26 @@ function love.load()
   love.physics.setMeter(64)
   world = love.physics.newWorld(0, 10 * love.physics.getMeter(), true)
 
-  local circle = love.physics.newBody(world, 200,10, 'dynamic')
-  love.physics.newFixture(circle, love.physics.newCircleShape(10))
+  -- dynamic
+  local body = love.physics.newBody(world, 200,10, 'dynamic')
+  love.physics.newFixture(body, love.physics.newCircleShape(10))
 
-  local polygon = love.physics.newBody(world, 150,150, 'static')
-  love.physics.newFixture(polygon, love.physics.newPolygonShape(0,0, 200,20, 200,60, 150,70))
+  -- static
+  body = love.physics.newBody(world, 150,150, 'static')
+  love.physics.newFixture(body, love.physics.newPolygonShape(0,0, 200,20, 200,60, 150,70))
+
+  -- sensor
+  body = love.physics.newBody(world, 250,250, 'static')
+  local fixture = love.physics.newFixture(body, love.physics.newPolygonShape(0,0, 200,0, 100,60))
+  fixture:setSensor(true)
+
+  -- edge
+  body = love.physics.newBody(world, 250, 300, 'static')
+  love.physics.newFixture(body, love.physics.newEdgeShape(0,0,300,-10))
+
+  -- chain
+  body = love.physics.newBody(world, 350, 400, 'static')
+  love.physics.newFixture(body, love.physics.newChainShape(false, 0,0,200,10,250,-20,350,30))
 
 end
 
